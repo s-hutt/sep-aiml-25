3b9397a90eaf97a24c7eddbf8e07397104947b98
Yan Liu <guccimanederzweite@gmail.com>
2025-06-02 20:59:45 +0200

    chore: reformatting using precommit.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,24 +5,24 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from __future__ import annotations
 
-project = 'shapiq_student'
-copyright = '2025, Sep-AIML-25-Group-2'
-author = 'Sep-AIML-25-Group-2'
-release = '0.1.1'
+project = "shapiq_student"
+copyright = "2025, Sep-AIML-25-Group-2"
+author = "Sep-AIML-25-Group-2"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]

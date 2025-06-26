cb4283166937a5eb60a89369df0a4141236f1d15
Yan Liu <guccimanederzweite@gmail.com>
2025-06-26 19:13:08 +0200

    refactor: new tests_grading files and default python modules are added for matching the new 1.3.0 version shapiq.

--- shapiq_student/__init__.py
+++ shapiq_student/__init__.py
@@ -1,8 +1,16 @@
 """Source code for the shapiq_student package."""
 
+from .gaussian_copula_imputer import GaussianCopulaImputer
+from .gaussian_imputer import GaussianImputer
+from .knn_explainer import KNNExplainer
+
 __version__ = "0.3.2"
 
 __all__ = [
     # version
     "__version__",
+    "KNNExplainer",
+    "GaussianCopulaImputer",
+    "GaussianImputer",
+    "subset_finding",
 ]

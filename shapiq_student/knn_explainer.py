a5aebb4d6aa667a346b13886bdfd6ccce5e59d36
Yan Liu <guccimanederzweite@gmail.com>
2025-07-04 00:29:25 +0200

    small fix by pre-commit

--- shapiq_student/knn_explainer.py
+++ shapiq_student/knn_explainer.py
@@ -2,16 +2,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from shapiq.interaction_values import InteractionValues
-    from sklearn.neighbors import KNeighborsClassifier
-
-import numpy as np
 from shapiq.explainer import Explainer
 
 
 class KNNExplainer(Explainer):
     """Klasse zur Bestimmung datenpunktbasierter Einflusswerte mithilfe des KNN-Verfahrens."""
-    pass
